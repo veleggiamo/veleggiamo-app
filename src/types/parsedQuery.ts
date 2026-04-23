078e095b013a922fcb327d6b3995f644af514cc8
veleggiamo <dropengoworld@gmail.com>
2026-04-23 20:40:57 +0200

    chore: remove legacy equipment marketplace code, install next-mdx-remote + gray-matter

--- src/types/parsedQuery.ts
+++
@@ -1,8 +0,0 @@
-export type Categoria = 'ancora' | 'autopilota' | 'energia'
-
-export interface ParsedQuery {
-  categoria: Categoria | null
-  lunghezzaBarca: number | null
-  rawQuery: string
-  confidence: number
-}
